9d51861b17e07e074a670b7fe897e5360da156b1
Артём Самуйлов <artemka.samuiloff@yandex.ru>
2025-05-09 12:18:59 +0300

    правки после командного ревью

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.service.FilmService;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
